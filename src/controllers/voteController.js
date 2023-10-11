b4a706fb2d71ae6eb5ae89511c191a610f82dde2
[Projet] <[r.marcelli31@gmail.com]>
2023-10-11 23:53:22 +0200

    Ajout Mail + Result

--- src/controllers/voteController.js
+++ src/controllers/voteController.js
@@ -1,6 +1,8 @@
 const Vote = require('../models/voteModel');
 const Musique = require('../models/musiqueModel');
 
+//GET (All)
+
 exports.listAllVotes = async (req, res) => {
     try {
         const votes = await Vote.find({ musique_id: req.params.id_musique });
@@ -12,11 +14,12 @@ exports.listAllVotes = async (req, res) => {
     }
 }
 
+//POST
+
 exports.createAVote = async (req, res) => {
     try {
-        const musique = await Musique.findById(req.params.id_musique);
+        const Musique = await Musique.findById(req.params.id_musique);
         const newVote = new Vote({ ...req.body, musique_id: req.params.id_musique });
-
         try {
             const vote = await newVote.save();
             res.status(201).json(vote);
@@ -32,6 +35,8 @@ exports.createAVote = async (req, res) => {
     }
 }
 
+// GET (ONE)
+
 exports.getAVote = async (req, res) => {
     try {
         const vote = await Vote.findById(req.params.id_vote);
@@ -40,10 +45,11 @@ exports.getAVote = async (req, res) => {
     } catch (error) {
         res.status(500);
         console.log(error);
-        res.json({ message: "Erreur serveur (getAvote)" })
+        res.json({ message: "Erreur serveur (getAVote)" })
     }
 }
 
+// PUTCH
 exports.updateAVote = async (req, res) => {
     try {
         const vote = await Vote.findByIdAndUpdate(req.params.id_vote, req.body, { new: true });
@@ -56,6 +62,7 @@ exports.updateAVote = async (req, res) => {
     }
 }
 
+//DELETE
 exports.deleteAVote = async (req, res) => {
     // const postDelete = new Post(req.body);
 
@@ -69,3 +76,21 @@ exports.deleteAVote = async (req, res) => {
         res.json({ message: 'Erreur serveur' });
     }
 };
+
+//RESULT
+exports.resultVote = async (req,res) => {
+    try{
+        const allVotes = await Vote.find({musique_id : req.params.id_musique});
+        console.log(allVotes);
+        let result = 0;
+        allVotes.forEach((vote)=>{
+            result += vote.level;
+        })
+        res.status(200);
+        res.json({ message: 'resultat : ' });
+    }catch (error) {
+        res.status(500);
+        console.log(error);
+        res.json({ message: 'Erreur serveur' });
+}
+}
